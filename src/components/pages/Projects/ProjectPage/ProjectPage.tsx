780333aa476be9904401442837d5d76189809620
Yuval Drori <yuvald29@protonmail.com>
2020-09-21 14:56:30 +0300

    modal video

--- src/components/pages/Projects/ProjectPage/ProjectPage.tsx
+++ src/components/pages/Projects/ProjectPage/ProjectPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Tooltip } from "@material-ui/core";
 
 import TechIcons from "./utils/consts/TechIcons";
@@ -6,6 +6,7 @@ import TechIcons from "./utils/consts/TechIcons";
 import "./ProjectPage.css";
 
 import external from "../../../../utils/types/external";
+import Modal from "../../../Modal/Modal";
 
 type tiKey = keyof typeof TechIcons;
 type techIcons = Array<tiKey>;
@@ -19,6 +20,22 @@ interface Props {
 }
 
 export default function ProjectPage(props: Props) {
+	const [vidsToShow, setVidsToShow] = useState<{ [index: number]: boolean }>(
+		{}
+	);
+	const [hasVids, toggleHasVids] = useState<boolean>(false);
+
+	useEffect(() => {
+		props.externals?.forEach((extObj: external, index: number) => {
+			if (extObj.title === "Video") {
+				if (!hasVids) toggleHasVids(true);
+				setVidsToShow((prevState) => {
+					return { ...prevState, [index]: false };
+				});
+			}
+		});
+	}, [props.externals, hasVids]);
+
 	const icons = useMemo((): JSX.Element[] => {
 		return props.techIcons.map((iconKey: tiKey, index: number) => {
 			const tiObj = TechIcons[iconKey];
@@ -35,19 +52,54 @@ export default function ProjectPage(props: Props) {
 		if (props.externals) {
 			const exts: JSX.Element[] = [];
 
-			props.externals.forEach((externalObj: external, index: number) => {
-				exts.push(
-					<a key={index} href={externalObj.url}>
-						{externalObj.title}
-					</a>
-				);
+			props.externals.forEach((extObj: external, index: number) => {
+				if (extObj.title !== "Video") {
+					exts.push(
+						<a key={index} href={extObj.url}>
+							{extObj.title}
+						</a>
+					);
+				} else {
+					exts.push(
+						<span
+							key={index}
+							onClick={() => {
+								console.log(vidsToShow);
+								setVidsToShow((prevState) => {
+									return { ...prevState, [index]: true };
+								});
+							}}
+						>
+							Video
+						</span>
+					);
+				}
 
 				if (index < props.externals!.length - 1) exts.push(<span> | </span>);
 			});
 
 			return exts;
 		}
-	}, [props.externals]);
+	}, [props.externals, vidsToShow]);
+
+	const vidModals = useMemo(() => {
+		const modals: JSX.Element[] = [];
+
+		if (props.externals) {
+			for (const vidIndex in vidsToShow) {
+				modals.push(
+					<Modal show={vidsToShow[vidIndex]}>
+						<video controls src={props.externals[vidIndex].url} />
+					</Modal>
+				);
+			}
+		}
+
+		return modals;
+	}, [vidsToShow, props.externals]);
+
+	console.log(vidModals);
+	console.log(vidsToShow);
 
 	return (
 		<div>
@@ -58,6 +110,7 @@ export default function ProjectPage(props: Props) {
 			<p className="pageExpl projectExpl">{props.desc}</p>
 			<div className="tiContainer">{icons}</div>
 			{externals ? <div className="extsContainer">{externals}</div> : null}
+			{vidModals}
 		</div>
 	);
 }
